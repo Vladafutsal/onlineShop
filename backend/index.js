63be938e4d2a68b9643ac3fe8a0502d64cd293c4
Vladimir Vukcevic <v.vukcevic@beg.aero>
2022-06-04 15:33:17 +0200

    backend setup and login API

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,34 @@
 const express = require('express');
-const  bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/dbConfig');
+const mongoose = require('mongoose');
+const Users = require('./models/userModel');
 
 const app = express();
-const router = express.Router();
+mongoose.connect(dbConfig.MONGODB_URL)
+    .then(data => console.log('MONGO DB is connected.'))
+    .catch(err => console.log(`Error while connecting to MONGO DB: ${err}`));
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-router.post('/api/login', (req, res) => {
-    res.send('Login API call is working.');
+app.post('/api/login', (req, res) => {
+    const reqBody = req.body;
+    console.log(reqBody);
+
+    const foundUser = Users.findOne(reqBody, (err, data) => {
+        console.log(data);
+        if (err) {
+            const errorMsg = `Error on getting user from DB: ${err}`;
+            console.log(errorMsg);
+            res.send(errorMsg);
+        }
+        else {
+            res.send(data);
+        }
+    });
 });
+
 app.listen(4000, err => {
     if (err) {
         console.log(err);
