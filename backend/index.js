0096fe113e3ba2621379f4cb6dd7a31cf1b95dba
Vladimir Vukcevic <v.vukcevic@beg.aero>
2022-06-06 21:10:35 +0200

    register new user

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const dbConfig = require('./config/dbConfig');
 const mongoose = require('mongoose');
 const Users = require('./models/userModel');
+const serverConfig = require('./config/serverConfig');
 
 const app = express();
 mongoose.connect(dbConfig.MONGODB_URL)
@@ -14,7 +15,6 @@ app.use(bodyParser.json());
 
 app.post('/api/login', (req, res) => {
     const reqBody = req.body;
-    console.log(reqBody);
 
     const foundUser = Users.findOne(reqBody, (err, data) => {
         console.log(data);
@@ -22,18 +22,54 @@ app.post('/api/login', (req, res) => {
             const errorMsg = `Error on getting user from DB: ${err}`;
             console.log(errorMsg);
             res.send(errorMsg);
+            return;
         }
+
+        // First way
+        // if (data)
+        //     res.send(data);
+        // else {
+        //     res.send('User not found.');
+        // }
+
+        // Second way
+        // res.send(data ? data : 'User not found.');
+
+        // Third way
+        res.send(data || 'User not found.');
+    });
+});
+
+app.post('/api/register', async (req,res) => {
+    const reqBody = req.body;
+    // console.log(reqBody);
+
+    Users.findOne(reqBody, async (err, data) => {
+        console.log(data);
+        if (err) {
+            const errorMsg = `Error on getting user from DB: ${err}`;
+            console.log(errorMsg);
+            res.send(errorMsg);
+            return;
+        }
+
+        if (data)
+            res.send(`User already exist: ${data.username}`);
         else {
-            res.send(data);
+            const newUser = new Users(reqBody);
+            const saveNewUser = await newUser.save();
+            console.log(saveNewUser);
+
+            res.send(saveNewUser || 'User not registered.');
         }
     });
 });
 
-app.listen(4000, err => {
+app.listen(serverConfig.port, err => {
     if (err) {
         console.log(err);
     }
     else {
-        console.log('Server is running on port: 4000');
+        console.log(serverConfig.serverMsg);
     }
 });
